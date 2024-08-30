82648cfbae619ad025409decea983c90f0d8b14d
Matthew Kaminski <kaminskimateusz7@gmail.com>
2024-08-29 23:29:28 -0400

    Add initial daisy ui

--- src/capsules/login_form.rs
+++ src/capsules/login_form.rs
@@ -179,7 +179,7 @@ fn login_form_capsule<G: Html>(
                             })
                             button (on:click = handle_forgot_password, class="text-sm text-blue-700 hover:underline dark:text-blue-500"){"Lost Password?"}
                         }
-                        button (on:click = handle_log_in, class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"){"Log in"}
+                        button (on:click = handle_log_in, class="btn"){"Log in"}
                         div (class="text-sm font-medium text-gray-500 dark:text-gray-300"){
                             button (on:click = handle_register, class="text-blue-700 hover:underline dark:text-blue-500"){"Create account"}
                         }
